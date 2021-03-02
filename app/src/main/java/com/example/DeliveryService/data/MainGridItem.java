36cea7c087bd15a37758550a85a4b334744de46b
Moony-H <moonylight247@gmail.com>
2021-03-03 00:15:45 +0900

    RecyclerView edit1

--- app/src/main/java/com/example/DeliveryService/data/MainGridItem.java
+++ app/src/main/java/com/example/DeliveryService/data/MainGridItem.java
@@ -1,10 +1,12 @@
 package com.example.DeliveryService.data;
 
 public class MainGridItem {
-    String name;
-    int image;
-    public MainGridItem(String name, int image){
+    public String name;
+    public int image;
+    public int tag;
+    public MainGridItem(String name, int image,int tag){
         this.name=name;
         this.image=image;
+        this.tag=tag;
     }
 }
--- app/src/main/java/com/example/DeliveryService/manager/MainGridAdapter.java
+++ app/src/main/java/com/example/DeliveryService/manager/MainGridAdapter.java
@@ -1,25 +1,44 @@
 package com.example.DeliveryService.manager;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.DeliveryService.R;
+import com.example.DeliveryService.data.MainGridItem;
+
+import java.util.ArrayList;
 
 public class MainGridAdapter extends RecyclerView.Adapter<MainGridAdapter.ViewHolder> {
+
+
+    ArrayList<MainGridItem> menus;
+
+    public MainGridAdapter(ArrayList<MainGridItem> menus){
+        this.menus=menus;
+    }
+
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
-        return null;
+        //아이템 레이아웃을 확장.
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.main_grid_item,parent,false);
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.image.setImageResource(menus.get(position).image);
+        holder.text.setText(menus.get(position).name);
+        holder.menuTag=menus.get(position).tag;
+
 
     }
 
